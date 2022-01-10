b41fd0c39236323bf1af21dbe0b22ac69f1d7a1c
RENAR Rébecca <Bombastic@Bombastic-CP.local>
2022-01-10 14:29:46 +0100

    Product, Category, Brand

---
+++ src/Entity/Category.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Product;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Brand;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?string
+    {
+        return $this->Product;
+    }
+
+    public function setProduct(string $Product): self
+    {
+        $this->Product = $Product;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->Brand;
+    }
+
+    public function setBrand(string $Brand): self
+    {
+        $this->Brand = $Brand;
+
+        return $this;
+    }
+}
